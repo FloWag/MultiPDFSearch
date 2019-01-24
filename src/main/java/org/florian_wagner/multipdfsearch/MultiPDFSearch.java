fc73ad4958276fb98ae1e64c98b3acb4865c8bdd
Florian Wagner <florianwagner.bremen@googlemail.com>
2019-01-24 17:07:27 +0100

    basic java setup

---
+++ src/main/java/org/florian_wagner/multipdfsearch/MultiPDFSearch.java
@@ -0,0 +1,15 @@
+package org.florian_wagner.multipdfsearch;
+
+public class MultiPDFSearch {
+
+    public static void main(String[] args)
+    {
+        new MultiPDFSearch().init();
+    }
+
+    public void init()
+    {
+
+    }
+
+}
